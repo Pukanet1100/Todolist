3d250f6f701b1f4b7f2017afeb59790485c3d602
BOSS <work@work.com>
2025-01-31 15:31:18 +0700

    Tutorial : create a feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import {
   NgIf,
   NgFor,
@@ -12,7 +13,8 @@ import {
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
